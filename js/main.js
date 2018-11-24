443cd921c3617934513e5a29669fe64fd1a7577f
naim nilger <naimnilger@gmail.com>
2018-11-25 00:39:38 +0530

    fix for sidebar

--- js/main.js
+++ js/main.js
@@ -94,24 +94,32 @@ $(window).on('load', function() {
 		hamburger_cross();
 	});
 
-	function hamburger_cross() {
+var trigger = $('.hamburger'),
+	overlay = $('.overlay'),
+	isClosed = false;
 
-		if (isClosed == true) {
-			overlay.hide();
-			trigger.removeClass('is-open');
-			trigger.addClass('is-closed');
-			isClosed = false;
-		} else {
-			overlay.show();
-			trigger.removeClass('is-closed');
-			trigger.addClass('is-open');
-			isClosed = true;
-		}
+trigger.click(function () {
+	hamburger_cross();
+});
+
+function hamburger_cross() {
+
+	if (isClosed == true) {
+		overlay.hide();
+		trigger.removeClass('is-open');
+		trigger.addClass('is-closed');
+		isClosed = false;
+	} else {
+		overlay.show();
+		trigger.removeClass('is-closed');
+		trigger.addClass('is-open');
+		isClosed = true;
 	}
+}
 
-	$('[data-toggle="offcanvas"]').click(function () {
-		$('#wrapper').toggleClass('toggled');
-	});
+$('[data-toggle="offcanvas"]').click(function () {
+	$('#wrapper').toggleClass('toggled');
+});
 	
 if($().circleProgress){
 
